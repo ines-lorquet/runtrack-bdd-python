af1ef63bbcb38bc46c183d317122a6ffcbbf9816
ines-lorquet <ines.lorquet@laplateforme.io>
2024-01-29 15:12:17 +0100

    Jour01 Runtrack bdd job05

--- jour01/job05.sql
+++ jour01/job05.sql
@@ -1,7 +1,7 @@
 -- Active: 1706528666952@@127.0.0.1@3306@laplateforme
 INSERT INTO etudiant(nom,prenom,age,email) VALUES
-    ('Spaghetti','Betty', 23,'betty.Spaghetti@laplateforme.io'),
-    ('Steak','Chuck', 45,'chuck.steak@laplateforme.io'),
+    ('Betty','Spaghetti', 23,'betty.Spaghetti@laplateforme.io'),
+    ('Chuck','Steak', 45,'chuck.steak@laplateforme.io'),
     ('Doe','John', 18,'john.doe@laplateforme.io'),
-    ('Barnes','Binkie', 16,'binkie.barnes@laplateforme.io'),
-    ('Gertrude','Dupuis', 20,'gertrude.dupuis@laplateforme.io');
+    ('Binkie','Barnes', 16,'binkie.barnes@laplateforme.io'),
+    ('Dupuis','Gertrude', 20,'gertrude.dupuis@laplateforme.io');
