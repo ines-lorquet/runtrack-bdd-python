ea6f2262e1388002f0189cda9339cbd87003688f
ines-lorquet <ines.lorquet@laplateforme.io>
2024-01-29 11:34:50 +0100

    Jour01 Runtrack bdd job03

--- jour01/job03.sql
+++ jour01/job03.sql
@@ -1,10 +1,10 @@
 USE LaPlateforme;
 
 CREATE TABLE etudiant (
-    -> id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    -> nom VARCHAR(255) NOT NULL,
-    -> prenom VARCHAR(255) NOT NULL,
-    -> age INT NOT NULL,
-    -> email VARCHAR(255) NOT NULL
-    -> );
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL
+    );
     
